47a2e5db28b14cd140eb918038c7fb1c4a1a915a
Gehad Moeiza <moeiza@newsletter2go.com>
2021-04-04 15:59:19 +0200

    App as class plus state and event

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 
 import Turtle from './Turtle/Turtle.js';
+import { Component } from 'react';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Turtle name="Leonardo" color="blue" power="90">The leader</Turtle>
-      <Turtle name="raphael" color="red" power="120">Hot head</Turtle>
-    </div>
-  );
-}
+// function App() {
 
+  class App extends Component {
+    // state change trigger re-render
+    state = {
+      turtles: [
+        {name: "Leonardo",color: "blue" ,power:90, desc: "The leader"},
+        {name: "raphael",color: "red" ,power:120, desc: "Hot head"}
+      ],
+      title: "TMNT"
+    };
+  
+    switchNameHandler = (newName) => {
+  // DON'T DO THAT (this.state.persons[0].name = "Nuha")    
+  //  notice warning in console, you need to use setState
+      
+          this.setState({
+            turtles: [
+              {name: "LEO!!",color: "blue" ,power:90, desc: "The leader"},
+              {name: "raphael",color: "red" ,power:120, desc: "Hot head"}
+            ]            
+        });
+    }
+  
+    render() {
+      return (
+        <div className="App">
+          <h1>{this.state.title}</h1>
+          <button onClick={this.switchNameHandler}>Switch Name</button>
+          <Turtle name={this.state.turtles[0].name} color="blue" power="90">The leader</Turtle>
+          <Turtle name="raphael" color="red" power="120">Hot head</Turtle>
+        </div>
+      );
+    }
+  }
 export default App;
+
+
