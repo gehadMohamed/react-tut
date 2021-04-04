9499040a16d40d9db101057f566a9f6e07dfdeca
Gehad Moeiza <moeiza@newsletter2go.com>
2021-04-04 14:44:03 +0200

    Add first component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Turtle from './Turtle/Turtle'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Turtle name="Leonardo" color="blue" power="90"/>
+      <Turtle name="raphael" color="red" power="120"/>
     </div>
   );
 }
