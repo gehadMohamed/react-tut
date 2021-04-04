6cd27821882a9783439cf7df0380e8fb920fe35d
Gehad Moeiza <moeiza@newsletter2go.com>
2021-04-04 16:19:48 +0200

    pass function as a reference

--- src/App.js
+++ src/App.js
@@ -11,17 +11,18 @@ function App() {
         {name: "Leonardo",color: "blue" ,power:90, desc: "The leader"},
         {name: "raphael",color: "red" ,power:120, desc: "Hot head"}
       ],
-      title: "TMNT"
+      title: ""
     }
   );
 
-  const switchNameHandler = () => {
+  const switchNameHandler = (name) => {
     // DON'T DO THAT (this.state.persons[0].name = "Nuha")    
     //  notice warning in console, you need to use setState
-        
+        const leoNickName = typeof name == 'string'? name : "LEO!!";    
+
         setTurtles({
             turtles: [
-              {name: "LEO!!",color: "blue" ,power:90, desc: "The leader"},
+              {name: leoNickName ,color: "blue" ,power:90, desc: "The leader"},
               {name: "raphael",color: "red" ,power:120, desc: "Hot head"}
             ]            
         });
@@ -29,9 +30,9 @@ function App() {
 
     return (
       <div className="App">
-        <h1>{turtlesState.title}</h1>
+        <h1>TMNT</h1>
         <button onClick={switchNameHandler}>Switch Name</button>
-        <Turtle name= {turtlesState.turtles[0].name} color="blue" power="90">The leader</Turtle>
+        <Turtle name= {turtlesState.turtles[0].name} color="blue" power="90" click={() => switchNameHandler('LeoN')}>The leader</Turtle>
         <Turtle name="raphael" color="red" power="120">Hot head</Turtle>
       </div>
   );
