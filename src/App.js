ce9adb019ed8f4b09852f2b152be78c4e7e9ac6d
Gehad Moeiza <moeiza@newsletter2go.com>
2021-04-04 16:08:00 +0200

    App as function with state hook

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
 
 import Turtle from './Turtle/Turtle.js';
-import { Component } from 'react';
+import { useState } from 'react';
 import './App.css';
 
-// function App() {
 
-  class App extends Component {
-    // state change trigger re-render
-    state = {
+function App() {
+  const [turtlesState, setTurtles] = useState(
+    {
       turtles: [
         {name: "Leonardo",color: "blue" ,power:90, desc: "The leader"},
         {name: "raphael",color: "red" ,power:120, desc: "Hot head"}
       ],
       title: "TMNT"
-    };
-  
-    switchNameHandler = (newName) => {
-  // DON'T DO THAT (this.state.persons[0].name = "Nuha")    
-  //  notice warning in console, you need to use setState
-      
-          this.setState({
+    }
+  );
+
+  const switchNameHandler = () => {
+    // DON'T DO THAT (this.state.persons[0].name = "Nuha")    
+    //  notice warning in console, you need to use setState
+        
+        setTurtles({
             turtles: [
               {name: "LEO!!",color: "blue" ,power:90, desc: "The leader"},
               {name: "raphael",color: "red" ,power:120, desc: "Hot head"}
             ]            
         });
-    }
-  
-    render() {
-      return (
-        <div className="App">
-          <h1>{this.state.title}</h1>
-          <button onClick={this.switchNameHandler}>Switch Name</button>
-          <Turtle name={this.state.turtles[0].name} color="blue" power="90">The leader</Turtle>
-          <Turtle name="raphael" color="red" power="120">Hot head</Turtle>
-        </div>
-      );
-    }
-  }
+      }
+
+    return (
+      <div className="App">
+        <h1>{turtlesState.title}</h1>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Turtle name= {turtlesState.turtles[0].name} color="blue" power="90">The leader</Turtle>
+        <Turtle name="raphael" color="red" power="120">Hot head</Turtle>
+      </div>
+  );
+}
+
+
 export default App;
 
 
