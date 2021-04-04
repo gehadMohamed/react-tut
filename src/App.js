54820c538f6f30e241e7ed2b5a6cde8330cf2269
Gehad Moeiza <moeiza@newsletter2go.com>
2021-04-04 14:54:09 +0200

    Using children

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Turtle from './Turtle/Turtle'
+import Turtle from './Turtle/Turtle';
 
 function App() {
   return (
     <div className="App">
-      <Turtle name="Leonardo" color="blue" power="90"/>
-      <Turtle name="raphael" color="red" power="120"/>
+      <Turtle name="Leonardo" color="blue" power="90">The leader</Turtle>
+      <Turtle name="raphael" color="red" power="120">Hot head</Turtle>
     </div>
   );
 }
--- src/Turtle/Turtle.js
+++ src/Turtle/Turtle.js
@@ -5,6 +5,7 @@ import React from 'react';
 const turtle = (props) => {
     return (
         <div className="turtle" style={{backgroundColor: props.color}}>
+            <label onClick={props.click}>{props.children} : </label>
             <h1>{props.name}</h1>
             <p>Power: {props.power}</p>   
       </div> 
